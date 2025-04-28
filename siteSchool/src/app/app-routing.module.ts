6ed42eeeffdc7e111222a816348d023b8b8c68a3
leSage1945 <koukodilaexauce@gmail.com>
2025-04-28 17:29:00 +0200

    creaetion de la page header, footer et paiement

--- siteSchool/src/app/app-routing.module.ts
+++ siteSchool/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaiementComponent } from './Paiement/paiement/paiement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: 'paiement', component: PaiementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- siteSchool/src/app/app.module.ts
+++ siteSchool/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaiementComponent } from './Paiement/paiement/paiement.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiceComponent } from './Paiement/service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiementComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServiceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
