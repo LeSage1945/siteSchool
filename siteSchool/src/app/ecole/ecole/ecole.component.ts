a5a1d35c000cf256f0e87ab2b12f507910049d44
leSage1945 <koukodilaexauce@gmail.com>
2025-05-30 11:39:15 +0200

    recherhe

--- siteSchool/src/app/ecole/ecole/ecole.component.ts
+++ siteSchool/src/app/ecole/ecole/ecole.component.ts
@@ -35,14 +35,26 @@ export class EcoleComponent {
     this.router.navigateByUrl('paiement/' + data.CodeEtab + '/' + data.NomEtab)
   }
 
-  get recherches(){
-    return this.ecoles.filter((elem: Iecole)=>{
-      if(elem.CodeEtab.toLocaleLowerCase().includes(this.recherche) || elem.NomEtab.toLocaleLowerCase().includes(this.recherche) || elem.Ville.toLocaleLowerCase().includes(this.recherche)){
-        return true
-      }
-      return false
-    })
-  }
+  // get recherches(){
+  //   return this.ecoles.filter((elem: Iecole)=>{
+  //     if(elem.CodeEtab.toLocaleLowerCase().includes(this.recherche) || elem.NomEtab.toLocaleLowerCase().includes(this.recherche) || elem.Ville.toLocaleLowerCase().includes(this.recherche)){
+  //       return true
+  //     }
+  //     return false
+  //   })
+  // }
+
+  get recherches() {
+  const terme = this.recherche.toLocaleLowerCase(); // normalisation du terme de recherche
+  return this.ecoles.filter((elem: Iecole) => {
+    return (
+      elem.CodeEtab.toLocaleLowerCase().includes(terme) ||
+      elem.NomEtab.toLocaleLowerCase().includes(terme) ||
+      elem.Ville.toLocaleLowerCase().includes(terme)
+    );
+  });
+}
+
 
 
 
